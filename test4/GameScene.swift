43d975e9ea306a1f32aad4dbc6d1f4e6ef43213c
ankitrajkaur <ankitrajkaur@gmail.com>
2019-10-17 14:31:45 -0400

    Refactor

--- test4/GameScene.swift
+++ test4/GameScene.swift
@@ -12,26 +12,21 @@ import GameplayKit
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
 // Adding sprites
-    var rabbit:SKSpriteNode!
-    let rabbit_Speed:CGFloat = 15
+    var player:SKSpriteNode!
+    let PLAYER_SPEED:CGFloat = 15
     
     override func didMove(to view: SKView) {
         self.physicsWorld.contactDelegate = self
         //initialisation of rabbit
-        self.rabbit = self.childNode(withName:"rabbit") as! SKSpriteNode
+        self.player = (self.childNode(withName: "player") as! SKSpriteNode)
+
     // set up physics for the rabbit
         
-        
         }
-        
-    
-    
-    
-   
     func didBegin(_ contact: SKPhysicsContact) {
         print("Something collided!")
     }
-}
+
  override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
     //func restartPlayer() {
@@ -43,44 +38,47 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     //addChild(rabbit)
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        let mouseTouch = touches.first
-               if (mouseTouch == nil) {
-                   return
-               }
-               let location = mouseTouch!.location(in: self)
+ override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+     // GET THE POSITION WHERE THE MOUSE WAS CLICKED
+     // ---------------------------------------------
+     let mouseTouch = touches.first
+     if (mouseTouch == nil) {
+         return
+     }
+     let location = mouseTouch!.location(in: self)
 
-               // WHAT NODE DID THE PLAYER TOUCH
-               // ----------------------------------------------
-               let nodeTouched = atPoint(location).name
-               //print("Player touched: \(nodeTouched)")
-               
-               
-               // GAME LOGIC: Move player based on touch
-               if (nodeTouched == "up") {
-                   // move up
-                   self.rabbit.position.y = self.player.position.y + rabbit_Speed
-               }
-               else if (nodeTouched == "down") {
-                   // move down
-                    self.rabbit.position.y = self.player.position.y - rabbit_Speed
-               }
-               else if (nodeTouched == "left") {
-                   // move left
-                    self.rabbit.position.x = self.player.position.x - rabbit_Speed
-               }
-               else if (nodeTouched == "right") {
-                   // move right
-                    self.rabbit.position.x = self.player.position.x + rabbit_Speed
-               }
-               
-           }
+     // WHAT NODE DID THE PLAYER TOUCH
+     // ----------------------------------------------
+     let nodeTouched = atPoint(location).name
+     //print("Player touched: \(nodeTouched)")
+     
+     
+     // GAME LOGIC: Move player based on touch
+     if (nodeTouched == "upButton") {
+         // move up
+         self.player.position.y = self.player.position.y + PLAYER_SPEED
+     }
+     else if (nodeTouched == "downButton") {
+         // move down
+          self.player.position.y = self.player.position.y - PLAYER_SPEED
+     }
+     else if (nodeTouched == "leftButton") {
+         // move left
+          self.player.position.x = self.player.position.x - PLAYER_SPEED
+     }
+     else if (nodeTouched == "rightButton") {
+         // move right
+          self.player.position.x = self.player.position.x + PLAYER_SPEED
+     }
+     
+ }
            
     
     
-func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-    }
-    
-func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-    }
+//func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+//    }
+//
+//func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+//    }
 
+}
